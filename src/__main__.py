519ec69a22a9cb26e37671cc9ee7ee13cebbbb63
Gaspard TOURDIAT <gaspard.tourdiat@gmail.com>
2026-01-27 00:07:47 +0100

    update (main) added base parsing

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python3
 
-import argparse
+try:
+    import argparse
+except Exception as e:
+    print(f'Could not import a module: {e}')
+    print('Have you tried installing the env with "make install"?')
+
+
+def main() -> None:
+    # ===== Parsing =====
+    parser = argparse.ArgumentParser(
+        description="gtourdia's call_me_maybe project.")
+    parser.add_argument('--input', required=True, help="input file path")
+    parser.add_argument('--output', required=True, help="output file path")
+    args = parser.parse_args()
+    print('DEBUG: ARGS', args)
+
+    return None
+
+
+if __name__ == '__main__':
+    main()
