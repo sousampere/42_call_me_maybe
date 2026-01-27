e950862c783de1a96886fd1b94d2ace24190e202
Gaspard Tourdiat <gaspard.tourdiat@gmail.com>
2026-01-27 19:10:49 +0100

    update (main) reorganized project up until llm init

--- src/__main__.py
+++ src/__main__.py
@@ -2,43 +2,68 @@
 
 try:
     import argparse
-    from src.constants import Colors
     from src.misc import printerr
     import json
+    from ..llm_sdk import Small_LLM_Model
 except Exception as e:
     print(f"Could not import a module: {e}")
     print('Have you tried installing the env with "make install"?')
-    exit()
+    exit(1)
 
 
-def main() -> None:
-    # ? ===== Parsing =====
-    parser = argparse.ArgumentParser(description="gtourdia's call_me_maybe project.")
+def load_json(json_file_path: str) -> list:
+    """ Loads the given json file as a list object """
+    try:
+        with open(json_file_path, 'r') as input_file:
+            loaded_json = json.load(input_file)
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f'Your input file was not found. {e}')
+    except Exception as e:
+        raise Exception(e)
+    return loaded_json
+
+
+def get_parsed_args() -> dict[str, str]:
+    """ Initialize argparse settings and returns input and output args """
+    parser = argparse.ArgumentParser(
+        description="gtourdia's call_me_maybe project (from school 42). "
+        "Usage : uv run python -m src [--input <input_file>] [--output <output_file>]"
+    )
     parser.add_argument(
         # Input arg
         "--input",
         required=True,
-        help="input file path containing the prompts to test",
+        help="input file path -> containing the prompts to test",
         default='data/input/function_calling_tests.json'
     )
     parser.add_argument(
         # Output arg
         "--output",
         required=True,
-        help="output file path",
+        help="output file path -> to store llm's results",
         default='data/output/function_calling_result.json'
     )
     args = parser.parse_args()
+    arguments_dict = {'input': args.input, 'output': args.output}
+    return arguments_dict
+
+
+def main() -> None:
+    """
+    Main function
+    - Getting input/output files (from args)
+    - Open input file
+    """
+    # ? ===== Parsing =====
+    args = get_parsed_args()
 
     # ? ===== Opening input file ======
-    try:
-        with open(args.input, 'r') as input_file:
-            json_input = json.load(input_file)
-            print(json_input)
-    except FileNotFoundError as e:
-        raise FileNotFoundError(f'Your input file was not found. {e}')
-    except Exception as e:
-        raise Exception(e)
+    json_data = load_json(args['input'])
+    for prompt in json_data:
+        print(prompt)
+
+    # ? ===== Calling LLM ======
+
     return None
 
 
--- src/misc.py
+++ src/misc.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 
 from src.constants import Colors
+import sys
 
 
 def printerr(*args) -> None:
     """ Print a red message """
-    print(f"{Colors.RED}", end='')
-    print(*args, end=f'{Colors.END}\n')
-
+    sys.stderr.write(f"{Colors.RED}")
+    sys.stderr.write(*args)
+    sys.stderr.write(f'{Colors.END}\n')
+    # print(f"{Colors.RED}", end='')
+    # print(*args, end=f'{Colors.END}\n')
 
 
 if __name__ == '__main__':
