dfaee399e0f2103c31f72cb02c2b10f66ba12246
Gaspard TOURDIAT <gaspard.tourdiat@gmail.com>
2026-02-03 14:52:19 +0100

    update (*) Working POC

--- src/__main__.py
+++ src/__main__.py
@@ -45,12 +45,16 @@ def main() -> None:
     Main function
     - Getting input/output files (from args)
     - Open input file
-    """    
+    """
+    # ? ===== Loading LLM ======
+    llm = Small_LLM_Model(device='mps')
+
     # ? ===== Parsing =====
     args = get_parsed_args()
 
     # ? ===== Opening input file ======
-    json_data = load_json(args['input'])
+    prompts = load_json(args['input'])
+    final_json = []
     # for prompt in json_data:
         # print(prompt['prompt'])
 
@@ -60,105 +64,107 @@ def main() -> None:
     avaiable_functions = list(map(lambda ft: ft['fn_name'], json_data_ft))
     print(avaiable_functions)
 
-    # ? ===== Prompt ======
-    user_prompt = 'Substitute the string "Hello world", by replacing "Hello" by "Goodbye".'
-    instructions = '''<|im_start|>system
+    for prompt in prompts:
+        # ? ===== Prompt ======
+        user_prompt = prompt['prompt']
+        instructions = '''<|im_start|>system
 {"available_functions": ''' + str(avaiable_functions) + ''',\
 "goal": "Select the correct function to execute the user's prompt"}<|im_end|>
 <|im_start|>user
 ''' + user_prompt + '''<|im_end|>
 <|im_start|>assistant
 '''
-    output = 'fn_'
-    printblue(instructions + output)
-    # ? ===== Preparing result =====
-    llm_result = {
-        'prompt': user_prompt
-    }
+        output = 'fn_'
+        printblue(instructions + output)
+        # ? ===== Preparing result =====
+        llm_result = {
+            'prompt': user_prompt
+        }
 
-    # ? ===== Loading LLM ======
-    llm = Small_LLM_Model(device='mps')
+        # ? ===== Tokenization ======
+        # Encoding the tokens
+        encoded_text = llm._encode(instructions + output)
+        encoded_text = tensor_to_list(encoded_text)
 
-    # ? ===== Tokenization ======
-    # Encoding the tokens
-    encoded_text = llm._encode(instructions + output)
-    encoded_text = tensor_to_list(encoded_text)
+        # ? ===== Calculating logits ======
+        logits = llm.get_logits_from_input_ids(encoded_text)
+        wordlist = llm.get_path_to_vocabulary_json()
+        print(wordlist)
 
-    # ? ===== Calculating logits ======
-    logits = llm.get_logits_from_input_ids(encoded_text)
-    wordlist = llm.get_path_to_vocabulary_json()
-    print(wordlist)
+        # ? ===== Generate valid token, one by one ======
+        while (output not in avaiable_functions):
+            print(output)
 
-    # ? ===== Generate valid token, one by one ======
-    while (output not in avaiable_functions):
-        print(output)
-
-        ft_list = []
-        for function in avaiable_functions:
-            if function.startswith(output):
-                ft_list.append(function)
-        if len(ft_list) == 1:
-            output = ft_list[0]
-            break
-        try:
-            # Getting highest token
-            current_token = get_highest_str_token_from_logits(logits, wordlist)
-            print(output+current_token)
-            valid_token = False
+            ft_list = []
             for function in avaiable_functions:
-                if function.startswith(output + current_token):
-                    # Generated token is valid
-                    output = output + current_token
-                    # Recalculating new logits
-                    encoded_text = llm._encode(instructions + output)
-                    encoded_text = tensor_to_list(encoded_text)
-                    logits = llm.get_logits_from_input_ids(encoded_text)
-                    valid_token = True
-                    break
-            if not valid_token:
-                # Skipping this token
+                if function.startswith(output):
+                    ft_list.append(function)
+            if len(ft_list) == 1:
+                output = ft_list[0]
+                break
+            try:
+                # Getting highest token
+                current_token = get_highest_str_token_from_logits(logits, wordlist)
+                print(output+current_token)
+                valid_token = False
+                for function in avaiable_functions:
+                    if function.startswith(output + current_token):
+                        # Generated token is valid
+                        output = output + current_token
+                        # Recalculating new logits
+                        encoded_text = llm._encode(instructions + output)
+                        encoded_text = tensor_to_list(encoded_text)
+                        logits = llm.get_logits_from_input_ids(encoded_text)
+                        valid_token = True
+                        break
+                if not valid_token:
+                    # Skipping this token
+                    logits = set_null_highest_token(logits)
+            except Exception as e:
+                print(e)
                 logits = set_null_highest_token(logits)
-        except Exception as e:
-            print(e)
-            logits = set_null_highest_token(logits)
-    llm_result['fn_name'] = output
-    llm_result['args'] = {}
+        llm_result['fn_name'] = output
+        llm_result['args'] = {}
 
-    # ? ===== Getting function args ======
-    function_data = get_function_data(output)
-    instructions = '''<|im_start|>system
-{"goal": "Select the arguments for the following function, according to the user's prompt, followed by a \n character."},\
-''' + str(function_data) + '''
-{"particularity": "For negative numbers, include a - character at the start"}<|im_end|>
-<|im_start|>user
-''' + user_prompt + '''<|im_end|>
-<|im_start|>assistant
-'''
-    # exit()
-    for arg in function_data['args_names']:
-        print(arg)
-        instructions = instructions + f'{arg}='
-        if function_data['args_types'][arg] == 'int':
-            print(f"{arg} is an int")
-            value = generate_int(instructions, llm)
-            llm_result['args'][arg] = value
-            instructions += str(value) + '\n'
-        if function_data['args_types'][arg] == 'float':
-            print(f"{arg} is a float")
-            value = generate_float(instructions, llm)
-            llm_result['args'][arg] = value
-            instructions += str(value) + '\n'
-        if function_data['args_types'][arg] == 'bool':
-            print(f"{arg} is a bool")
-            value = generate_bool(instructions, llm)
-            llm_result['args'][arg] = value
-            instructions += str(value) + '\n'
-        if function_data['args_types'][arg] == 'str':
-            print(f"{arg} is a str")
-            value = generate_str(instructions, llm)
-            llm_result['args'][arg] = value
-            instructions += str(value) + '\n'
-    print(llm_result)
+        # ? ===== Getting function args ======
+        function_data = get_function_data(output)
+        instructions = '''<|im_start|>system
+    {"goal": "Select the arguments for the following function, according to the user's prompt, followed by a \n character."},\
+    ''' + str(function_data) + '''
+    {"particularity": "For negative numbers, include a - character at the start"}<|im_end|>
+    <|im_start|>user
+    ''' + user_prompt + '''<|im_end|>
+    <|im_start|>assistant
+    '''
+        # exit()
+        for arg in function_data['args_names']:
+            print(arg)
+            instructions = instructions + f'{arg}='
+            if function_data['args_types'][arg] == 'int':
+                print(f"{arg} is an int")
+                value = generate_int(instructions, llm)
+                llm_result['args'][arg] = value
+                instructions += str(value) + '\n'
+            if function_data['args_types'][arg] == 'float':
+                print(f"{arg} is a float")
+                value = generate_float(instructions, llm)
+                llm_result['args'][arg] = value
+                instructions += str(value) + '\n'
+            if function_data['args_types'][arg] == 'bool':
+                print(f"{arg} is a bool")
+                value = generate_bool(instructions, llm)
+                llm_result['args'][arg] = value
+                instructions += str(value) + '\n'
+            if function_data['args_types'][arg] == 'str':
+                print(f"{arg} is a str")
+                value = generate_str(instructions, llm)
+                llm_result['args'][arg] = value
+                instructions += str(value) + '\n'
+        print(llm_result)
+        final_json.append(llm_result)
+    print(final_json)
+    with open('json_output.json', 'w') as f:
+        json.dump(final_json, f)
     return None
 
 
