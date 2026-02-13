7dca43f06b6a3d1826f0c52dbedddbd02bf4fc2e
Gaspard Tourdiat <gaspard.tourdiat@gmail.com>
2026-02-13 16:12:46 +0100

    update (prompts) removed comment

--- src/prompts.py
+++ src/prompts.py
@@ -19,14 +19,6 @@ def get_function_instructions(
 def get_args_instructions(
         function_data: list[Any],
         user_prompt: str) -> str:
-    # output = '<|im_start|>system \n'\
-    # '{"goal": "Select '\
-    # 'the arguments for the following function,'\
-    # 'according to the user\'s prompt, followed by a \n character."}'\
-    # f' {str(function_data)} ''{"particularity": "For negative '\
-    # 'numbers, include a - character at the start"}<|im_end|>'\
-    # f'<|im_start|>user {user_prompt} <|im_end|>'\
-    # '<|im_start|>assistant\n'
     instructions = '''<|im_start|>system
 {"goal": "Select the arguments for the following function, \
 according to the user's prompt, followed by a \n character."},\
